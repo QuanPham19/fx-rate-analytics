64cc27525562eefa30aee929f3a4d656b175ed0e
QuanPham19 <106662700+QuanPham19@users.noreply.github.com>
2024-06-22 14:21:59 +0000

    Update changes

--- chrome/install.sh
+++ chrome/install.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
-echo "Hello World"
+rm google-chrome-stable_current_amd64.deb
