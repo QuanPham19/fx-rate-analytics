caad10545b334165a71c7ab9eae330ec48f79c3c
QuanPham19 <106662700+QuanPham19@users.noreply.github.com>
2024-06-22 16:38:02 +0000

    Update changes

--- chrome/install.sh
+++ chrome/install.sh
@@ -1,6 +1,9 @@
-export PATH=$PATH:/usr/bin
-sudo apt update
-sudo apt install wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
-rm google-chrome-stable_current_amd64.deb
+#!/bin/bash
+set -eux
+apt-get install -y wget
+wget -qO /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+apt-get install -y -f
+rm /tmp/google-chrome-stable_current_amd64.deb
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
