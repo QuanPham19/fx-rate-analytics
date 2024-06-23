822650779cadacca79af09e2261275f9b9657392
QuanPham19 <106662700+QuanPham19@users.noreply.github.com>
2024-06-23 06:25:03 +0000

    Update changes

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ import subprocess
 def run_multiple_commands():
     commands = [
         "su - root",  
-        "sudo --version"
+        "apt install sudo",
+        "apt-get install -y libglib2.0-0=2.50.3-2 \
+            libnss3=2:3.26.2-1.1+deb9u1 \
+            libgconf-2-4=3.2.6-4+b1 \
+            libfontconfig1=2.11.0-6.7+b1"
     ]
     try:
         for command in commands:
