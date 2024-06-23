c72b8f461b1befbc4b8bca7c42423ceefa6cb159
QuanPham19 <106662700+QuanPham19@users.noreply.github.com>
2024-06-23 03:46:28 +0000

    Update changes

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from scripts.auto import ExchangeRateAnalytics
 from scripts.wise import wise_scraping
 from scripts.wu import wu_scraping
 
-wise_df = wise_scraping()
-wu_df = wu_scraping()
+# wise_df = wise_scraping()
+# wu_df = wu_scraping()
 
 @flow(log_prints=True)
 def run_exchange_rate_analytics():
-    test = ExchangeRateAnalytics(
-        wise_df=wise_df,
-        wu_df=wu_df,
-        corridor_dir='input/sending_receiving_country_pair.xlsx',
-        output_dir='output.xlsx'
-    )
-    test.run()
+    # test = ExchangeRateAnalytics(
+    #     wise_df=wise_df,
+    #     wu_df=wu_df,
+    #     corridor_dir='input/sending_receiving_country_pair.xlsx',
+    #     output_dir='output.xlsx'
+    # )
+    # test.run()
+    print('Hello World')
 
     
