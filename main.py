99e7b788539ddd1629c1adf00f92be00a2ff05fc
QuanPham19 <106662700+QuanPham19@users.noreply.github.com>
2024-06-23 05:52:17 +0000

    Update changes

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ from prefect import task, flow
 wise_df = wise_scraping()
 wu_df = wu_scraping()
 
+@task(log_prints=True)
+def get_os():
+    command = "cat /etc/*-release"
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print(result.stdout)
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
 @flow(log_prints=True)
 def run_exchange_rate_analytics():
+    get_os()
     test = ExchangeRateAnalytics(
         wise_df=wise_df,
         wu_df=wu_df,
